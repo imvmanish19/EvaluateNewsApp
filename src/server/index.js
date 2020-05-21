20fd559e26d438494f9acda2dd76a649529e00a4
Manish V <imvmanish19@gmail.com>
2020-05-21 15:21:08 +0530

    dotenv config done

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
+var aylien = require("aylien_textapi");
 const mockAPIResponse = require('./mockAPI.js')
+
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY_ALY
+});
 
 const app = express()
 
-app.use(express.static('dist'))
+app.use(express.static('src'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('../client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
